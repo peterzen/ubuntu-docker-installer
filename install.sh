7b98c4bf55ffbde45d79556ea2ef69fa6b759163
Peter Banik <peter@prioritylane.com>
2017-02-25 08:58:44 +0100

    script to install Docker on Ubuntu

---
+++ install.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+
+sudo apt-get update && sudo apt-get -y upgrade
+
+sudo apt-get install -y --no-install-recommends \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+	
+	
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+	   
+	   
+sudo apt-get update && sudo apt-get -y install docker-engine
+
+echo
+echo * Docker install completed
+echo
+
+sudo docker run hello-world
+
